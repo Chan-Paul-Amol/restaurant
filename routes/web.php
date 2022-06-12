25dbd981e2b6329caec9bc9a5460936342357aad
maryom99 <99712544+maryom99@users.noreply.github.com>
2022-06-12 17:22:02 +0700

    PagesController & index page & Route(M)

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/book', 'PagesController@book')->name('book');
+
+/*Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+*/
